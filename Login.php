2a403f80ce1dfbf981282757762fdc02b2908839
samy <technicalhacker899@gmail.com>
2021-02-03 17:46:35 +0530

    Login-Form
    Login File Moved

---
+++ Login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Gradient Glowing Login Form</title>
+    <link rel="stylesheet" href="working.css">
+    </head>    
+    <body>
+    <div class="container">
+        <div class="form">
+            <h2>Login Form</h2>
+            <form>
+               <div class="inputBox">
+                <input type="text" name=""
+                       placeholder="Username">
+                </div>
+                <div class="inputBox">
+                <input type="password" name=""
+                       placeholder="password">
+                </div>
+                 <div class="inputBox">
+                <input type="submit" value="Login">
+                </div>
+                <p>Forget Password ? <a href="#">
+                    Click Here</a></p>
+            </form>
+        </div>
+        </div>
+    </body>
+</html>
